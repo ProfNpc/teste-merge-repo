c8f8b4ad0b64a4b5771fa14d887e7453b48eccd4
Alessandro Tacini <alessandro.tacini@usp.br>
2025-04-30 10:31:51 -0300

    Versão Inicial

---
+++ src/main/java/com/example/testemerge/TestemergeApplication.java
@@ -0,0 +1,13 @@
+package com.example.testemerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestemergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestemergeApplication.class, args);
+	}
+
+}
